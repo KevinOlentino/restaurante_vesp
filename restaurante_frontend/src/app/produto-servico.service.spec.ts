28ba8f5e854acc4c2782ce30d7b51aa5588a26d2
lgoncalves <leonardo.14199@hotmail.com>
2021-07-30 17:32:43 -0400

    implementacaoTestesProduto

--- restaurante_frontend/src/app/produto-servico.service.spec.ts
+++ restaurante_frontend/src/app/produto-servico.service.spec.ts
@@ -1,5 +1,6 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { Produto } from './listar-produto/Produto';
 
 import { ProdutoServicoService } from './produto-servico.service';
 
@@ -13,7 +14,25 @@ describe('ProdutoServicoService', () => {
     service = TestBed.inject(ProdutoServicoService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+
+  it('ccontarProduto', () => {
+    let retorno: Number;
+    retorno = service.ccontarProduto();
+    expect(retorno).toEqual(41);
   });
+
+  it('descontoProduto', () => {
+    let produto: Produto = {codigo: 0, nome: 'Bala de coco', preco: 100.00, quantidade : 3};
+    let retorno : Number;
+    retorno = service.descontoProduto(produto);
+    expect(retorno).toEqual(90);
+  });
+
+  it('faltaProduto', () => {
+    let produto: Produto = {codigo: 0, nome: 'Bala de limao', preco: 100.00, quantidade : 3};
+    let retorno : boolean;
+    retorno = service.faltaProduto(produto);
+    expect(retorno).toBeTruthy;
+  });
+
 });
