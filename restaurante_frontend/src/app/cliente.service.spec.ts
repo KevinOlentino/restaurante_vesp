1372c28bc98724e84aee7c838d1e496ffc9a62e0
Sidney Maldonado <sidneymaldonado@outlook.com>
2021-07-30 13:38:29 -0400

    testes

--- restaurante_frontend/src/app/cliente.service.spec.ts
+++ restaurante_frontend/src/app/cliente.service.spec.ts
@@ -1,9 +1,11 @@
+import { HttpClient, HttpHandler } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { ClienteService } from './cliente.service';
 
 describe('ClienteService', () => {
   let service: ClienteService;
+  let service2: ClienteService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
