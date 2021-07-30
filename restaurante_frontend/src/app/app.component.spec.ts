18101dfe6f1e127f89a6df5ebe399f3c16bf4c25
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2021-07-30 15:39:45 -0400

    testes frontend

--- restaurante_frontend/src/app/app.component.spec.ts
+++ restaurante_frontend/src/app/app.component.spec.ts
@@ -1,6 +1,5 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
-import { RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
