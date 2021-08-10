6fa9de357b6b588e5537677137673fd98efa3e1c
MariaVictoria00 <mariavictoriapaybe@gmail.com>
2021-08-10 15:57:10 -0400

    ajuste

--- restaurante_frontend/src/app/app-routing.module.ts
+++ restaurante_frontend/src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
 {path: 'incluirsetor', component: IncluirSetorComponent},
 {path: 'listarsetor', component: ListarSetorComponent},
 {path: 'incluirveiculo', component: IncluirVeiculoComponent},
-{path: 'listarveiculo', component: ListarVeiculoComponent}
+{path: 'listarveiculo', component: ListarVeiculoComponent},
 
 {path: 'listarfornecedor', component: ListarFornecedorComponent},
 {path: 'incluirfornecedor', component: IncluirFornecedorComponent}
